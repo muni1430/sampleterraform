dc72db2e88375624fa57d78318c7252b131963d0
muni1430 <93640586+muni1430@users.noreply.github.com>
2022-01-05 16:01:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,6 @@
 provider "aws" {
   region = "${var.aws_region}"
 }
-module "vpc" {
-  source = "../modules/vpc"
-  aws_region = "${var.aws_region}"
-  environment = "${var.environment}"
-  vpc_cidr = "${var.vpc_cidr}"
-
-  private_subnet_1 = "${var.private_subnet_1}"
-  private_subnet_2 = "${var.private_subnet_2}"
-  private_subnet_3 = "${var.private_subnet_3}"
-
-  dmz_subnet_1 = "${var.dmz_subnet_1}"
-  dmz_subnet_2 = "${var.dmz_subnet_2}"
-  dmz_subnet_3 = "${var.dmz_subnet_3}"
-}
 resource "aws_security_group" "beacon" {
   name = "beacon"
   description = "Allow web traffic from the internet"
@@ -27,12 +13,6 @@ resource "aws_security_group" "beacon" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
-resource "aws_eip" "beacon" {
-  instance = "${aws_instance.beacon.id}"
-  vpc = true
-}
-
 resource "aws_instance" "beacon" {
   ami = "ami-04505e74c0741db8d"
   instance_type = "t2.micro"
@@ -40,7 +20,7 @@ resource "aws_instance" "beacon" {
   security_groups = "sg-0025ec649683001e8"
  
   tags {
-    Name = "beacon-${var.environment}"
-    Environment = "${var.environment}"
+    Name = "muni"
+   
   }
 }
