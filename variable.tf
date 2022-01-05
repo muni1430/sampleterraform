3eaadfc76471cfecfcbfb818bbcf0f8cd4115134
muni1430 <93640586+muni1430@users.noreply.github.com>
2022-01-05 20:40:11 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -3,9 +3,9 @@ variable "aws_region" {
   default = "us-east-1"
 }
 # VPC module variables
-variable "availability_zone " {
+variable "availability_zone" {
   type    = string
-  default = ["us-east-1"]
+  default = ["us-east-1c"]
 }
 variable "aws_instance" {
   description = "aws to be used on EC2 instance created"
