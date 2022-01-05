efe1cbcccc16ddc55c7c3c3114f1551dbb4a1fc0
muni1430 <93640586+muni1430@users.noreply.github.com>
2022-01-05 20:38:26 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,22 +1,17 @@
 # AWS provider variables
-
 variable "aws_region" {
   default = "us-east-1"
 }
-
 # VPC module variables
-
 variable "availability_zone " {
   type    = string
   default = ["us-east-1"]
 }
-
 variable "aws_instance" {
   description = "aws to be used on EC2 instance created"
   type        = string
   default     = ""
 }
-
 variable "ami" {
   description = "ID of AMI to use for the instance"
   type        = string
