c129e6ae8696e3489105eb8415328a4f077acbed
muni1430 <93640586+muni1430@users.noreply.github.com>
2022-01-05 20:12:58 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -5,13 +5,11 @@ variable "aws_region" {
 }
 
 # VPC module variables
-variable "environment" {}
-variable "vpc_cidr" {}
+variable "image_id" {
+  type = string
+}
 
-variable "private_subnet_1" {}
-variable "private_subnet_2" {}
-variable "private_subnet_3" {}
-
-variable "dmz_subnet_1" {}
-variable "dmz_subnet_2" {}
-variable "dmz_subnet_3" {}
+variable "availability_zone_names" {
+  type    = string
+  default = ["us-east-1"]
+}
