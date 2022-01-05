6420147b745609741d70c96150ffef9951c69841
muni1430 <93640586+muni1430@users.noreply.github.com>
2022-01-05 19:09:27 +0530

    Update module.tf

--- module.tf
+++ module.tf
@@ -1,8 +1,15 @@
-provider "aws" {
-  region = "${var.aws_region}"
+provider "aws"{
+	profile = "default"
+	region = "us-east-1"
 }
-module "vpc" {
-  source = "../modules/vpc"
-  aws_region = "${var.aws_region}"
-  environment = "${var.environment}"
-}
+resource "aws_instance" "web" {
+     ami = "ami-04505e74c0741db8d"
+     instance_type = "t2.micro"
+     availability_zone = "us-east-1c"
+	 key_name = "muneendar54"
+	 subnet_id = "subnet-0991fd811575b0586"
+	tags = {
+	name = "hello world"
+	}
+ }
+
